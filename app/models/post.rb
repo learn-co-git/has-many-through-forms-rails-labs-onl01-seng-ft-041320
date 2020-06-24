0d908d9f479281bdc22251a82a5e828d98e28527
Neil Empey <argus.two.2@gmail.com>
2020-06-24 07:28:34 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,13 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(categories_attributes)
-    categories_attributes.values.each do |category_attributes|
-      if category_attributes[:name].present?
-        category = Category.find_or_create_by(category_attributes)
-        if !self.categories.include?(category)
-          self.post_categories.build(:category => category)
-        end
+  categories_attributes.values.each do |category_attributes|
+  if category_attributes[:name].present?
+     category = Category.find_or_create_by(category_attributes)
+    if !self.categories.include?(category)
+     self.post_categories.build(:category => category)
       end
+     end
     end
   end
-
 end
